75cd5aa9da59f3c73e2bc314e5dbba64c0cc4659
Billy Robert O'Neal III <bion@microsoft.com>
2026-05-04 20:05:37 -0700

    Rename to gistrec-geo-utils, use VCPKG_BUILD_TYPE, add NOTICE.

--- ports/gistrec-geo-utils/portfile.cmake
+++ ports/gistrec-geo-utils/portfile.cmake
@@ -1,3 +1,5 @@
+set(VCPKG_BUILD_TYPE release) # header-only
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gistrec/geo-utils
@@ -16,10 +18,8 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/GeoUtils PACKAGE_NAME GeoUtils)
 
-# Header-only — no compiled artifacts to keep
 file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/debug"
     "${CURRENT_PACKAGES_DIR}/lib"
 )
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE" "${SOURCE_PATH}/NOTICE")
